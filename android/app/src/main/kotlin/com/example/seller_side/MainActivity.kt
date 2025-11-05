c0afaaa68a3e6ca63c70fca25e0d53a3098a2a78
donzsky07 <phyncx26@gmail.com>
2025-11-05 23:15:57 +0800

    Add remaining files

---
+++ android/app/src/main/kotlin/com/example/seller_side/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.seller_side
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/consts/strings.dart
@@ -0,0 +1,8 @@
+const appname = 'ACraft Seller App', 
+  login = 'Login', 
+  welcome = 'Hi!, Welcome to', 
+  email = 'Email', 
+  password = 'Password', 
+  forgotPassword = 'Forgot Password', 
+  anyProblem ='in case of difficulty, contact adminisration',
+  credit = '@santurdioDevs';
